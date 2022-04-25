6616e3ce4121a55a1e728cadea8bc495a92c0375
jarvisqi <qdw8911@foxmail.com>
2022-04-25 16:56:30 +0800

    重写route 定义路由

--- gee/gee.go
+++ gee/gee.go
@@ -1,7 +1,6 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,16 +8,15 @@ import (
 type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
@@ -36,10 +34,6 @@ func (engine *Engine) Run(addr string) (err error) {
 
 // 解析请求的路径，查找路由映射表
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	ctx := newContext(w, req)
+	engine.router.handle(ctx)
 }
